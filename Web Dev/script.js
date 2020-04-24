01ea47347049a8f1a3441f6d474152933efe8f7a
Calapox <64170970+Calapox@users.noreply.github.com>
2020-04-24 04:55:22 +0100

    update

--- Web Dev/script.js
+++ Web Dev/script.js
@@ -1,1 +1,39 @@
-/* Javascript place holder */
+$(document).ready(function(){
+
+    $('.navi a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top -100,
+        },500,'linear'
+      )
+    })
+
+    $('.navi a').click( function() {
+        $(this).parent().siblings().children().removeClass('active');
+        $(this).addClass('active');
+    });
+
+})
+
+
+$(window).on('scroll', function(){
+    if ($(window).scrollTop()){
+        $('nav').addClass('scrollBar');
+    }
+    else {
+        $('nav').removeClass('scrollBar');
+    }
+    
+
+    
+});
+
+
+
+
+
+
+
+
+
+
